5f56a519b9ea593f6d7e91cad61bd11302d90b1b
omursnck <omrsnr12@icloud.com>
2023-01-27 22:19:31 +0300

    Initial Commit

---
+++ WhatsappClone/WhatsappCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappCloneApp.swift
+//  WhatsappClone
+//
+//  Created by Ömür Şenocak on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
